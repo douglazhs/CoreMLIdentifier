10ee5aa823ecae63826ae5087a349cb6f23b17c5
Douglas Henrique de Souza Pereira <50143403+douglazhs@users.noreply.github.com>
2022-06-03 17:29:11 -0300

    initial commit

---
+++ CoreMLIObjectIdentifier/CoreMLIObjectIdentifier/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CoreMLIObjectIdentifier
+//
+//  Created by Douglas Henrique de Souza Pereira on 03/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
